5c123c4c0d77bd53f2c580fad37d9a9f9476f56d
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-04-28 09:52:44 -0300

    Deletar Pet

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -1,11 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
+import { toast } from "react-hot-toast";
 
 export function Pets() {
   const [pets, setPets] = useState(null);
+  const [show, setShow] = useState(false);
+  const [idPet, setIdPet] = useState(null);
+
+  const handleClose = () => {
+        setIdPet(null);
+        setShow(false)
+    };
+    const handleShow = (id) => {
+        setIdPet(id);
+        setShow(true)
+    };
 
   useEffect(() => {
     initializeTable();
@@ -21,6 +33,19 @@ export function Pets() {
         console.log(error);
       });
   }
+
+  function onDelete() {
+        axios.delete(`http://localhost:3001/pets/${idPet}`)
+            .then(response => {
+                toast.success(response.data.message, { position: "bottom-right", duration: 2000 });
+                initializeTable();
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.response.data.message, { position: "bottom-right", duration: 2000 });
+            });
+        handleClose();
+    }
 
   return (
     <div className="clientes container">
@@ -38,6 +63,9 @@ export function Pets() {
             <tr>
               <th>Nome</th>
               <th>Tipo</th>
+              <th>Porte</th>
+              <th>Data de Nascimento</th>              
+              <th>ID Dono</th>              
               <th>Ações</th>
             </tr>
           </thead>
@@ -47,15 +75,18 @@ export function Pets() {
                 <tr key={pet.id}>
                   <td>{pet.nome}</td>
                   <td>{pet.tipo}</td>
+                  <td>{pet.porte}</td>
+                  <td>{pet.dataNasc}</td>
+                  <td>{pet.clienteId}</td>
                   <td className="d-flex gap-2">
-                    <Button>
+                    <Button onClick={() => handleShow(pet.id)}>
                       <i className="bi bi-trash-fill"></i>
                     </Button>
-                    <Button as={Link} to={`/pets/adicionar/${pet.id}`}>
+                    <Button as={Link} to={`/pets/editar/${pet.id}`}>
                       <i className="bi bi-pencil-fill"></i>
                     </Button>
-                    <Button as={Link} to={`/pets/detalhe/${pet.id}`}>
-                    <i class="bi bi-info-square"></i>
+                    <Button as={Link} to={`/pets/detalhes/${pet.id}`}>
+                    <i class="bi bi-info-square text-warning"></i>
                     </Button>
                   </td>
                 </tr>
@@ -64,6 +95,20 @@ export function Pets() {
           </tbody>
         </Table>
       )}
+      <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirmação</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Tem certeza que deseja excluir o pet?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleClose}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={onDelete}>
+                        Excluir
+                    </Button>
+                </Modal.Footer>
+            </Modal>
     </div>
   );
 }
