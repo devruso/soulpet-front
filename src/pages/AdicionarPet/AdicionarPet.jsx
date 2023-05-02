780848700530371a2b0e3e4b237f05e84ea3bd34
Stephany-Canuto <stephany.canutos25@gmail.com>
2023-05-02 10:15:06 -0300

    correção CSS

--- src/pages/AdicionarPet/AdicionarPet.jsx
+++ src/pages/AdicionarPet/AdicionarPet.jsx
@@ -36,14 +36,14 @@ export function AdicionarPet() {
   }
 
   return (
-    <div className="container-form">
+    <div className="container-formulario">
       <Row>
         <Col xs={5}>
           <img className="img-form" src={ImgPETform} alt="LOGO" />
         </Col>
         <Col>
-        <h1>Adicionar Novo Pet</h1>
-          <Form onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="titleForm">Adicionar Novo Pet</h1>
+          <Form onSubmit={handleSubmit(onSubmit)} className="formAdd">
             <Form.Group className="mb-3">
               <Form.Label>Nome</Form.Label>
               <Form.Control
