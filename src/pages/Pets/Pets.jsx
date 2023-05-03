4b0be7a28950f8333ac9a217b06f247adb4daa7d
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-05-03 10:43:09 -0300

    Paginação pet front

--- src/pages/Pets/Pets.jsx
+++ src/pages/Pets/Pets.jsx
@@ -4,48 +4,65 @@ import { Button, Modal, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
 import { toast } from "react-hot-toast";
+import { Pagination } from "../../components/Pagination/Pagination";
 
 export function Pets() {
   const [pets, setPets] = useState(null);
-  const [show, setShow] = useState(false);
-  const [idPet, setIdPet] = useState(null);
+const [currentPage, setCurrentPage] = useState(1);
+const [totalPages, setTotalPages] = useState(1);
+const [show, setShow] = useState(false);
+const [idPet, setIdPet] = useState(null);
+const limit = 5; // define o número máximo de itens por página
+  
 
   const handleClose = () => {
-        setIdPet(null);
-        setShow(false)
-    };
-    const handleShow = (id) => {
-        setIdPet(id);
-        setShow(true)
-    };
+    setIdPet(null);
+    setShow(false);
+  };
+  const handleShow = (id) => {
+    setIdPet(id);
+    setShow(true);
+  };
 
   useEffect(() => {
     initializeTable();
-  }, []);
+  }, [currentPage]);
 
   function initializeTable() {
     axios
-      .get("http://localhost:3001/pets")
+    .get(`http://localhost:3001/pets?page=${currentPage}&limit=${limit}`)
       .then((response) => {
-        setPets(response.data);
+        setPets(response.data.listaPets);
+        setTotalPages(response.data.totalPages);
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
+
   function onDelete() {
-        axios.delete(`http://localhost:3001/pets/${idPet}`)
-            .then(response => {
-                toast.success(response.data.message, { position: "bottom-right", duration: 2000 });
-                initializeTable();
-            })
-            .catch(error => {
-                console.log(error);
-                toast.error(error.response.data.message, { position: "bottom-right", duration: 2000 });
-            });
-        handleClose();
-    }
+    axios
+      .delete(`http://localhost:3001/pets/${idPet}`)
+      .then((response) => {
+        toast.success(response.data.message, {
+          position: "bottom-right",
+          duration: 2000,
+        });
+        initializeTable();
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.response.data.message, {
+          position: "bottom-right",
+          duration: 2000,
+        });
+      });
+    handleClose();
+  }
 
   return (
     <div className="clientes container">
@@ -58,57 +75,65 @@ export function Pets() {
       {pets === null ? (
         <Loader />
       ) : (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Tipo</th>
-              <th>Porte</th>
-              <th>Data de Nascimento</th>              
-              <th>ID Dono</th>              
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pets.map((pet) => {
-              return (
-                <tr key={pet.id}>
-                  <td>{pet.nome}</td>
-                  <td>{pet.tipo}</td>
-                  <td>{pet.porte}</td>
-                  <td>{pet.dataNasc}</td>
-                  <td>{pet.clienteId}</td>
-                  <td className="d-flex gap-2">
-                    <Button onClick={() => handleShow(pet.id)}>
-                      <i className="bi bi-trash-fill"></i>
-                    </Button>
-                    <Button as={Link} to={`/pets/editar/${pet.id}`}>
-                      <i className="bi bi-pencil-fill"></i>
-                    </Button>
-                    <Button as={Link} to={`/pets/detalhes/${pet.id}`}>
-                    <i class="bi bi-info-square text-warning"></i>
-                    </Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+        <>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Tipo</th>
+                <th>Porte</th>
+                <th>Data de Nascimento</th>
+                <th>ID Dono</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pets.map((pet) => {
+                return (
+                  <tr key={pet.id}>
+                    <td>{pet.nome}</td>
+                    <td>{pet.tipo}</td>
+                    <td>{pet.porte}</td>
+                    <td>{pet.dataNasc}</td>
+                    <td>{pet.clienteId}</td>
+                    <td className="d-flex gap-2">
+                      <Button onClick={() => handleShow(pet.id)}>
+                        <i className="bi bi-trash-fill"></i>
+                      </Button>
+                      <Button as={Link} to={`/pets/editar/${pet.id}`}>
+                        <i className="bi bi-pencil-fill"></i>
+                      </Button>
+                      <Button as={Link} to={`/pets/detalhes/${pet.id}`}>
+                        <i className="bi bi-info-square text-warning"></i>
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <Pagination
+      total={totalPages}
+      currentPage={currentPage}
+      onChangePage={handlePageChange}
+    />
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirmação</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Tem certeza que deseja excluir o pet?</Modal.Body>
+            <Modal.Footer>
+              <Button variant="danger" onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button variant="primary" onClick={onDelete}>
+                Excluir
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </>
       )}
-      <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Confirmação</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Tem certeza que deseja excluir o pet?</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="danger" onClick={handleClose}>
-                        Cancelar
-                    </Button>
-                    <Button variant="primary" onClick={onDelete}>
-                        Excluir
-                    </Button>
-                </Modal.Footer>
-            </Modal>
     </div>
   );
 }
